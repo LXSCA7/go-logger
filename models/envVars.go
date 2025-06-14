5db5809317e8eb0b41986a3d9f33b94c3f3f5402
LXSCA <lucas.edr27@gmail.com>
2025-06-14 00:35:58 -0300

    feat: new fields

--- models/envVars.go
+++ models/envVars.go
@@ -1,10 +1,12 @@
 package models
 
 type EnvVars struct {
-	ApiPort string
-	ApiKey  string
-	DbHost  string
-	DbUser  string
-	DbPass  string
-	DbName  string
+	ApiPort    string
+	ApiKey     string
+	DbHost     string
+	DbUser     string
+	DbPass     string
+	DbName     string
+	DbPort     string
+	DbTimeZone string
 }
