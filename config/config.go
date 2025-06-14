c9062e92ab74881fb2f6e3dacbdf8c6270778bae
LXSCA <lucas.edr27@gmail.com>
2025-06-14 00:23:19 -0300

    feat all env vars

--- config/config.go
+++ config/config.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	"errors"
+	"fmt"
 	"log"
 	"os"
 
@@ -18,22 +18,39 @@ func LoadEnvVars() (*models.EnvVars, error) {
 	}
 
 	envVars := &models.EnvVars{
-		DbHost: os.Getenv("DB_HOST"),
-		DbPass: os.Getenv("DB_PASS"),
-		DbUser: os.Getenv("DB_USER"),
+		ApiPort: os.Getenv("API_PORT"),
+		ApiKey:  os.Getenv("API_KEY"),
+		DbHost:  os.Getenv("DB_HOST"),
+		DbUser:  os.Getenv("DB_USER"),
+		DbPass:  os.Getenv("DB_PASS"),
+		DbName:  os.Getenv("DB_NAME"),
 	}
 
-	if envVars.DbHost == "" {
-		return &models.EnvVars{}, errors.New("Required environmennt variable 'DB_HOST' not found.")
+	if err := validateEnv(envVars.ApiPort, "API_PORT"); err != nil {
+		return nil, err
 	}
-
-	if envVars.DbPass == "" {
-		return &models.EnvVars{}, errors.New("Required environmennt variable 'DB_PASS' not found.")
+	if err := validateEnv(envVars.ApiKey, "API_KEY"); err != nil {
+		return nil, err
 	}
-
-	if envVars.DbUser == "" {
-		return &models.EnvVars{}, errors.New("Required environmennt variable 'DB_USER' not found.")
+	if err := validateEnv(envVars.DbHost, "DB_HOST"); err != nil {
+		return nil, err
+	}
+	if err := validateEnv(envVars.DbUser, "DB_USER"); err != nil {
+		return nil, err
+	}
+	if err := validateEnv(envVars.DbPass, "DB_PASS"); err != nil {
+		return nil, err
+	}
+	if err := validateEnv(envVars.DbName, "DB_NAME"); err != nil {
+		return nil, err
 	}
 
 	return envVars, nil
+}
+
+func validateEnv(value string, key string) error {
+	if value == "" {
+		return fmt.Errorf("Required environment variable '%s' was not found.", key)
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -4,13 +4,16 @@ import (
 	"fmt"
 
 	"github.com/LXSCA7/go-logger/config"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	app := fiber.New()
 	vars, err := config.LoadEnvVars()
 	if err != nil {
 		panic(err.Error())
 	}
 
 	fmt.Println(vars.DbHost, vars.DbPass, vars.DbUser)
+	app.Listen(vars.ApiPort)
 }
