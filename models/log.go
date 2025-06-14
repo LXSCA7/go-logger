9b7b86191aca11cd694991a73f1f6e0917173e68
LXSCA <lucas.edr27@gmail.com>
2025-06-14 12:58:13 -0300

    feat: func to convert log payload to log

--- models/log.go
+++ models/log.go
@@ -1,17 +1,36 @@
 package models
 
 import (
+	"encoding/json"
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/datatypes"
 )
 
 type Log struct {
-	ID              uint   `gorm:"primaryKey"`
-	ApplicationName string `gorm:"index"`
-	Level           string `gorm:"index"`
+	ID              uuid.UUID `gorm:"primaryKey"`
+	ApplicationName string    `gorm:"index"`
+	Level           string    `gorm:"index"`
 	Message         string
 	StatusCode      int
 	Metadata        datatypes.JSON
 	CreatedAt       time.Time
+}
+
+func NewLogFromPayload(logPayload *LogPayload) (*Log, error) {
+	jsonBytes, err := json.Marshal(logPayload.Metadata)
+	if err != nil {
+		return nil, err
+	}
+
+	metadataJSON := datatypes.JSON(jsonBytes)
+	return &Log{
+		ID:              uuid.New(),
+		ApplicationName: logPayload.ApplicationName,
+		Level:           logPayload.Level,
+		StatusCode:      logPayload.StatusCode,
+		Message:         logPayload.Message,
+		Metadata:        metadataJSON,
+	}, nil
 }
